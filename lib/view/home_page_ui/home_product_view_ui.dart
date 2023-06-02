858fd208668ee25bb59bdc56dcf12b363ee0fc2a
fadilappu <fadilpachoos@gmail.com>
2023-06-02 11:58:56 +0530

    productview

--- lib/view/home_page_ui/home_product_view_ui.dart
+++ lib/view/home_page_ui/home_product_view_ui.dart
@@ -4,94 +4,90 @@ import '../../model/hive_model_class.dart';
 import '../product_ui/prouct_details_page_ui.dart';
 
 class HomeProductView extends StatelessWidget {
-  final String searchQuery;
+ final String searchQuery;
 
-  const HomeProductView({Key? key, required this.searchQuery}) : super(key: key);
+const HomeProductView({Key? key,required this.searchQuery}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // Filter the products based on the search query
+     // Filter the products based on the search query
     final List<Product> filteredProducts = products.where((product) {
       final productName = product.name.toLowerCase();
       final search = searchQuery.toLowerCase();
       return productName.contains(search);
     }).toList();
-
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: GridView.builder(
-        padding: const EdgeInsets.all(8),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 5,
-          mainAxisSpacing: 10,
-          crossAxisSpacing: 2,
-          mainAxisExtent: 300,
-        ),
-        itemCount: filteredProducts.length,
-        itemBuilder: (context, index) {
-          final Product product = filteredProducts[index];
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ProductDetailsPage(product),
-                  ),
-                );
-              },
-              child: Container(
-                margin: const EdgeInsets.all(10),
-                padding: const EdgeInsets.all(4),
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 167, 206, 239),
-                  borderRadius: const BorderRadius.only(
-                    bottomLeft: Radius.circular(8),
-                    bottomRight: Radius.circular(8),
-                  ),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      child: Image.network(
-                        product.imageUrls,
-                        height: 100,
-                        width: 200,
-                        fit: BoxFit.fill,
+    return  Padding(
+            padding:  const EdgeInsets.all(8.0),
+            child: GridView.builder(
+              padding:  const EdgeInsets.all(8),
+              gridDelegate:  const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                  mainAxisSpacing: 8,
+                  crossAxisSpacing: 2,
+                  // childAspectRatio: 10.5,
+                  mainAxisExtent: 311),
+              itemCount: filteredProducts.length,
+              itemBuilder: (context, index) {
+                final Product product = filteredProducts[index];
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ProductDetailsPage(product),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.all(16),
+                      padding: const EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[400],
+                        borderRadius: BorderRadius.circular(8),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(
-                            product.name,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
+                          SizedBox(
+                            child: Image.network(
+                              product.imageUrls,
+                              height: 200,
+                              width: double.infinity,
+                              fit: BoxFit.fill,
                             ),
                           ),
-                          const SizedBox(height: 4),
-                          Text(
-                            '\$${product.price.toStringAsFixed(2)}',
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 14,
+                          Padding(
+                            padding: const EdgeInsets.all(8),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  product.name,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                                const SizedBox(height: 4),
+                                Text(
+                                  '\$${product.price.toStringAsFixed(2)}',
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
                         ],
                       ),
                     ),
-                  ],
-                ),
-              ),
+                  ),
+                );
+              },
             ),
           );
-        },
-      ),
-    );
   }
 }
